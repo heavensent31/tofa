ad81352599008d10712169635a38f49cef321af2
Emil Viesná <regnveig@yandex.ru>
2022-10-17 12:25:44 +0700

    WTF:)

--- fast_commit.sh
+++ fast_commit.sh
@@ -1,5 +1,3 @@
-Timestamp=$( date -Iseconds; )
-Hostname=$( hostname; )
 git add *
-git commit -a -m "Fast commit from ${Hostname} at ${Timestamp}" -Sregnveig@yandex.ru
+git commit -a -m "${1}" -Sregnveig@yandex.ru
 git push
