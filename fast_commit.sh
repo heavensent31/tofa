8312daeef298e048f8f9488be96887db91126171
Emil Viesná <regnveig@yandex.ru>
2023-01-04 22:30:05 +0700

    #

--- fast_commit.sh
+++ fast_commit.sh
@@ -1,4 +1,9 @@
+# git remote add github git@github.com:regnveig/tofa.git
+# git remote add 0xacab git@0xacab.org:regnveig/tofa.git
+# git remote add darktea git@it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion:regnveig/tofa.git
+
 git add *
 git commit -a -m "${1}" -Sregnveig@yandex.ru
-git push all
-torify git push tor
+git push github
+git push 0xacab
+torify git push darktea
