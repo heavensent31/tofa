f963c5ea00bdca616f2e23fa16b06defd0129936
Emil Viesná <regnveig@yandex.ru>
2023-02-26 01:23:53 +0700

    compile.sh

--- src/ffog/compile.sh
+++ src/ffog/compile.sh
@@ -1,6 +1,11 @@
-for image in by cc cover_front_ru cover_front_en cover_back publisher_logo_ru publisher_logo_en;
+for image in cover_front_ru cover_front_en cover_back;
 do {
 	inkscape --export-text-to-path --export-filename=${image}.pdf ${image}.svg;
+} done;
+
+for image in by cc publisher_logo_ru publisher_logo_en;
+do {
+	inkscape --export-text-to-path --export-filename=${image}.pdf ../../svg/${image}.svg;
 } done;
 
 for image in by cc cover_front_ru cover_front_en cover_back publisher_logo_ru publisher_logo_en;
