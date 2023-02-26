60532030cf7ff38fd42a13570d02d35719b60934
Emil Viesná <regnveig@yandex.ru>
2023-02-26 23:46:15 +0700

    Gotcha little ffog!

---
+++ src/ffog/clear.sh
@@ -0,0 +1,5 @@
+for image in by cc cover_front_ru cover_front_en cover_back publisher_logo_ru publisher_logo_en;
+do {
+	 rm ${image}.pdf;
+} done;
+ 
--- src/ffog/compile.sh
+++ src/ffog/compile.sh
@@ -8,7 +8,7 @@ do {
 	inkscape --export-text-to-path --export-filename=${image}.pdf ../../svg/${image}.svg;
 } done;
 
-for image in by cc cover_front_ru cover_front_en cover_back publisher_logo_ru publisher_logo_en;
-do {
-	 rm ${image}.pdf;
-} done;
+#for tex in header_ru header_en;
+#do {
+#	xelatex -interaction=nonstopmode ${tex}.tex;
+#} done;
