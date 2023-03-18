5592de221a3e024e9228bf68054c133ebee762a5
Emil Viesná <regnveig@yandex.ru>
2023-03-19 01:45:50 +0700

    add one more repo; paranoia mode on

--- fast_commit.sh
+++ fast_commit.sh
@@ -1,9 +1,11 @@
 # git remote add github git@github.com:regnveig/tofa.git
 # git remote add 0xacab git@0xacab.org:regnveig/tofa.git
 # git remote add darktea git@it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion:regnveig/tofa.git
+# git remote add codeberg git@codeberg.org:regnveig/tofa.git
 
 git add *
 git commit -a -m "${1}" -Sregnveig@yandex.ru
 git push github
 git push 0xacab
+git push codeberg
 torify git push darktea
