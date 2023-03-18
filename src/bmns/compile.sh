7c5f6ebffeed274fe65389faa45dc3200c5a5c34
Emil Viesná <regnveig@yandex.ru>
2023-03-18 20:01:57 +0700

    too fa

---
+++ src/bmns/compile.sh
@@ -0,0 +1,4 @@
+for image in by cc tw publisher_logo_ru publisher_logo_en qr_github_tofa qr_license_by40;
+do {
+	inkscape --export-text-to-path --export-filename=${image}.pdf ../../svg/${image}.svg;
+} done;
