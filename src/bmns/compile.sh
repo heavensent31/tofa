321beaf15fd40b54deca38f5b9789febeb9bf2a7
Emil Viesná <regnveig@yandex.ru>
2023-03-26 16:43:50 +0700

    images!

--- src/bmns/compile.sh
+++ src/bmns/compile.sh
@@ -1,3 +1,8 @@
+for image in pic_HathComparison;
+do {
+	inkscape --export-text-to-path --export-filename=${image}.pdf ${image}.svg;
+} done;
+
 for image in by cc tw publisher_logo_ru publisher_logo_en qr_github_tofa qr_license_by40;
 do {
 	inkscape --export-text-to-path --export-filename=${image}.pdf ../../svg/${image}.svg;
--- src/snap/compile.sh
+++ src/snap/compile.sh
@@ -1,8 +1,3 @@
-for image in pic_MitchoeOldBook;
-do {
-	inkscape --export-text-to-path --export-filename=${image}.pdf ${image}.svg;
-} done;
-
 for image in by cc tw publisher_logo_ru publisher_logo_en qr_github_tofa qr_license_by40;
 do {
 	inkscape --export-text-to-path --export-filename=${image}.pdf ../../svg/${image}.svg;
